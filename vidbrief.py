57ef421609d8d98c0c24ab67561907896942dbe6
Nato <anjosrejuniorwork@gmail.com>
2026-03-30 17:43:50 -0300

    Add new menu options

--- vidbrief.py
+++ vidbrief.py
@@ -1,52 +1,91 @@
 import os
+
 from handlers.download_audio_h import run_download_audio
-from handlers.transcribe_audio_h import run_transcribe_audio
 from handlers.resume_transcription_h import run_resume_transcription
+from handlers.transcribe_audio_h import run_transcribe_audio
+
 
 def display_banner():
-    """Exibe o nome do projeto em ASCII Art estilizado."""
-    os.system('cls' if os.name == 'nt' else 'clear')
-    
+    """Project name in ASCII Art."""
+    os.system("cls" if os.name == "nt" else "clear")
+
     banner = """
-    __     ___     _ ____  _     _      __ 
+    __     ___     _ ____  _     _      __
     \ \   / (_) __| | __ )| \__(_) ___ / _|
-     \ \ / /| |/ _` |  _ \| '__| |/ _ \ |_ 
+     \ \ / /| |/ _` |  _ \| '__| |/ _ \ |_
       \ V / | | (_| | |_) | |  | |  __/  _|
-       \_/  |_|\__,_|____/|_|  |_|\___|_|  
-                                           
+       \_/  |_|\__,_|____/|_|  |_|\___|_|
+
     >>> Video Intelligence Tool <<<
     """
     print(banner)
 
+
 def main_menu():
     while True:
         display_banner()
-        
-        print(" [1] Transcribe Audio (YouTube URL)")
+
+        print(" [1] Download Audio (YouTube URL)")
+        print(" [2] Transcribe Audio")
+        print(" [3] Resume Audio")
+        print(" [4] Download, Transcribe & Summarize")
         print(" [0] Exit")
-        print("\n" + "_"*40)
-        
+        print("\n" + "_" * 40)
+
         choice = input("\n[VidBrief] Select an option: ").strip()
 
-        if choice == '1':
-            print("\n" + "="*40)
+        if choice == "1":
+            print("\n" + "=" * 40)
             url = input("[VidBrief] Enter YouTube URL: ")
-            
-            # Aqui você chamará suas funções de processamento:
+
             print(f"\n[VidBrief] Initializing process for: {url}")
-            print("[VidBrief] Please wait...")
-            
+            print("[VidBrief] Please wait... Download Audio")
             run_download_audio(url)
-            run_transcribe_audio()
-            run_resume_transcription()
-            
+            print("[VidBrief] ✅ Audio Downloaded")
+
             input("\n[VidBrief] Process finished. Press Enter to return to menu...")
             
-        elif choice == '0':
+        elif choice == "2":
+            print("\n" + "=" * 40)
+            print("\n[VidBrief] Please wait... Transcribing audio...")
+            run_transcribe_audio()
+            print("\n[VidBrief] ✅ Audio Transcribed")
+
+            input("\n[VidBrief] Process finished. Press Enter to return to menu...")
+
+        elif choice == "3":
+            print("\n" + "=" * 40)
+            print("\n[VidBrief] Please wait... Resuming transcription...")
+            run_resume_transcription()
+            print("\n[VidBrief] ✅ Transcription resumed")
+
+            input("\n[VidBrief] Process finished. Press Enter to return to menu...")
+            
+        elif choice == "4":
+            print("\n" + "=" * 40)
+
+            print(f"\n[VidBrief] Initializing process for: {url}")
+            print("[VidBrief] Please wait... Downloading Audio")
+            run_download_audio(url)
+            print("[VidBrief] ✅ Audio Downloaded")
+
+            print("\n[VidBrief] Transcribing audio...")
+            run_transcribe_audio()
+            print("\n[VidBrief] ✅ Audio Transcribed")
+
+            print("\n[VidBrief] Resuming transcription...")
+            run_resume_transcription()            
+            print("\n[VidBrief] ✅ Transcription resumed")
+
+            input("\n[VidBrief] Process finished. Press Enter to return to menu...")
+
+        elif choice == "0":
             print("\n[VidBrief] Exiting VidBrief... See you soon!")
             break
+
         else:
             input("\n[VidBrief] Invalid option! Press Enter to try again...")
+
 
 if __name__ == "__main__":
     main_menu()
